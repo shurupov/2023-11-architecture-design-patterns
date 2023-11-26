f54a05da32862bb5ea3634c17b1c4588c183295d
shurupov <bolteg86@ya.ru>
2023-11-26 20:17:53 +0400

    01-unit-testing test of zero a coefficient

--- 01-unit-testing/src/test/java/ru/shurupov/otus/architecture/equation/SquareEquationSolverTest.java
+++ 01-unit-testing/src/test/java/ru/shurupov/otus/architecture/equation/SquareEquationSolverTest.java
@@ -1,8 +1,10 @@
 package ru.shurupov.otus.architecture.equation;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
+import ru.shurupov.otus.architecture.equation.exception.CoefficientAEqualsZeroException;
 
 class SquareEquationSolverTest {
 
@@ -23,5 +25,11 @@ class SquareEquationSolverTest {
     assertThat(SquareEquationSolver.solve(1d, 2d, 1d, 0.001d))
         .usingComparatorWithPrecision(0.001)
         .containsExactlyInAnyOrder(-1d);
+  }
+
+  @Test
+  void givenACoefficientEqualsZero_whenSolveExecuted_thenExceptionThrown() {
+    assertThatThrownBy(() -> SquareEquationSolver.solve(0d, 2d, 1d, 0.001d))
+        .isOfAnyClassIn(CoefficientAEqualsZeroException.class);
   }
 }
